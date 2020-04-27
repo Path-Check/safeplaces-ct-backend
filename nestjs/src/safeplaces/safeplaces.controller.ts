55f94c3f122164093ee24b2f0e5e1c92b584d0e3
Pablo Moran <pablom@Pablos-MacBook-Pro.local>
2020-04-27 07:28:50 -0700

    added auth required for 3 endpoints

--- nestjs/src/safeplaces/safeplaces.controller.ts
+++ nestjs/src/safeplaces/safeplaces.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common'
+import { Controller, Post, Body, Get, Param, UseGuards } from '@nestjs/common'
 import { SafePlacesService } from './safeplaces.service'
 import { GetUser } from '../auth/decorators/get-user'
 import { SaveRedactedDto } from './types/payload/saveRedacted.dto'
@@ -7,6 +7,7 @@ import { LoadRes } from './types/response/load.interface'
 import { PublishDto } from './types/payload/publish.dto'
 import { PublishRes } from './types/response/publish.interface'
 import { SafePathsJsonRes } from './types/response/safePathsJson.interface'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller()
 export class SafePlacesController {
@@ -14,6 +15,7 @@ export class SafePlacesController {
 
   // https://github.com/tripleblindmarket/safe-places/blob/develop/Safe-Places-Server.md#save-redacted
   @Post('/redacted_trail')
+  @UseGuards(AuthGuard)
   saveRedactedTrail(
     @Body() payload: SaveRedactedDto,
     @GetUser() user // TODO: needs type, we also need to decide if this comes from the logged in user, or something else
@@ -23,12 +25,14 @@ export class SafePlacesController {
 
   //https://github.com/tripleblindmarket/safe-places/blob/develop/Safe-Places-Server.md#load-all-redacted
   @Get('/redacted_trail')
+  @UseGuards(AuthGuard)
   loadAllRedacted(@GetUser() user): Promise<LoadRes> {
     return this.safePlacesService.loadAllRedacted(user)
   }
 
   //https://github.com/tripleblindmarket/safe-places/blob/develop/Safe-Places-Server.md#publish
   @Post('/safe_paths')
+  @UseGuards(AuthGuard)
   publish(@Body() payload: PublishDto, @GetUser() user): Promise<PublishRes> {
     return this.safePlacesService.publish(payload, user)
   }
