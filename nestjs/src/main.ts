d6bc7aaed80b997fe3db29341ad04b5f009cda23
Pablo Moran <pablom@Pablos-MacBook-Pro.local>
2020-04-27 17:06:10 -0700

    added port to the config file

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -5,6 +5,7 @@ import * as helmet from 'helmet'
 
 import { AppModule } from './app.module'
 import { NormalizeInterceptor } from './interceptors/Normalize'
+import { port } from './config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -21,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('spec', app, document)
 
-  await app.listen(3000)
+  await app.listen(port)
 }
 bootstrap()
