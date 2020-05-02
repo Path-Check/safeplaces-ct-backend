cffbf2ddc063c99c7fb23ecbcad218610c45aedd
krishnadurai <krishnadurai20@gmail.com>
2020-05-02 19:40:40 +0530

    Concrete implementations for /safe_paths APIs POST & GET

--- expressjs/db/models/organizations.js
+++ expressjs/db/models/organizations.js
@@ -10,6 +10,17 @@ function findOne(filter){
   return Organizations().where(filter).first().then((row) => row);
 }
 
+function update(organization){
+  let organizationRecord = {};
+  organizationRecord.authority_name = organization.authority_name;
+  organizationRecord.info_website = organization.info_website;
+  organizationRecord.safe_path_json = organization.safe_path_json;
+  return Organizations()
+    .where({id: organization.id})
+    .update(organizationRecord).returning('*');
+}
+
 module.exports = {
-  findOne: findOne
+  findOne: findOne,
+  update: update
 };
