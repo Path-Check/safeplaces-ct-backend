0f3f8682f137f3cc7d961f2724c07bfe6e7a5a2b
Pablo Moran <pablom@Pablos-MacBook-Pro.local>
2020-05-07 02:33:10 -0700

    registered the org module

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from './auth/auth.module'
 import { SafePlacesModule } from './safeplaces/safeplaces.module'
 import { dbConfig } from './config'
+import { OrganizationModule } from './organization/organization.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbConfig), AuthModule, SafePlacesModule],
+  imports: [
+    TypeOrmModule.forRoot(dbConfig),
+    AuthModule,
+    SafePlacesModule,
+    OrganizationModule
+  ],
   controllers: [],
   providers: []
 })
