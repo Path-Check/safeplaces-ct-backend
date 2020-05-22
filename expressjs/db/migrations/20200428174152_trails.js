362f1c36c8972be6f7da7b0ba600e88e947defe9
Aiyan <48393820+aiyan@users.noreply.github.com>
2020-05-22 09:22:13 -0400

    Reformat code and enforce code style

--- expressjs/db/migrations/20200428174152_trails.js
+++ expressjs/db/migrations/20200428174152_trails.js
@@ -1,18 +1,20 @@
 const { onUpdateTrigger } = require('../../knexfile');
 
-exports.up = function(knex) {
-  return knex.schema.createTable("trails", table => {
-      table.increments("id").primary();
-      table.string("redacted_trail_id").notNullable();
-      table.uuid("user_id").notNullable();
-      table.uuid("organization_id").notNullable();
-      table.specificType("coordinates", "geometry(point, 4326)");
-      table.timestamp("time");
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('trails', table => {
+      table.increments('id').primary();
+      table.string('redacted_trail_id').notNullable();
+      table.uuid('user_id').notNullable();
+      table.uuid('organization_id').notNullable();
+      table.specificType('coordinates', 'geometry(point, 4326)');
+      table.timestamp('time');
       table.timestamps(true, true);
-    }).then(() => knex.raw(onUpdateTrigger('trails')));
+    })
+    .then(() => knex.raw(onUpdateTrigger('trails')));
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   let dropQuery = `DROP TABLE trails`;
   return knex.raw(dropQuery);
 };
