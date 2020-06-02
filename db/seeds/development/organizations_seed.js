124d301de82bb412954b699a5407efd3848880c6
Jeffrey Hunter <sublet@me.com>
2020-06-01 21:15:31 -0400

    Fixing seed data

--- db/seeds/development/organizations_seed.js
+++ db/seeds/development/organizations_seed.js
@@ -1,15 +1,21 @@
 exports.seed = function (knex) {
   return knex('organizations')
-    .del() // Deletes ALL existing entries
+    .del()
     .then(async function () {
-      // Inserts seed entries one by one in series
-      return knex('organizations').insert({
+      
+      await knex('organizations').insert({
+        id: 1,
+        name: 'Dev Organization'
+      });
+
+      await knex('settings').insert({
         id: 'a88309c2-26cd-4d2b-8923-af0779e423a3',
-        name: 'Dev Organization',
+        organization_id: 1,
         info_website_url:
           'https://www.who.int/emergencies/diseases/novel-coronavirus-2019',
         api_endpoint_url:
           'https://api.something.give/safe_path/',
       });
+
     });
 };
