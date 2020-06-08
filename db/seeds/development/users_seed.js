cd10679580183615e746d899ba7c2ba29fa89311
Daniel McCoy <dan.mccoy@rocketinsights.com>
2020-06-08 14:34:19 -0400

    add tester user to db seeds

--- db/seeds/development/users_seed.js
+++ db/seeds/development/users_seed.js
@@ -8,13 +8,24 @@ exports.seed = function (knex) {
     .del() // Deletes ALL existing entries
     .then(async function () {
       // Inserts seed entries one by one in series
-      let password = await bcrypt.hash('admin', 5);
-      return knex('users').insert({
+      let adminPassword = await bcrypt.hash('admin', 5);
+      await knex('users').insert({
         id: 'a88309ca-26cd-4d2b-8923-af0779e423a3',
         organization_id: 1,
         username: 'spladmin',
-        password: password,
+        password: adminPassword,
         email: 'admin@org.com',
+        is_admin: true,
+        maps_api_key: process.env.SEED_MAPS_API_KEY,
+      });
+
+      let testerPassword = await bcrypt.hash('theforce', 5);
+      await knex('users').insert({
+        id: 'a0845a0b-988b-43a7-b9f1-5fd6cac2cb12',
+        organization_id: 2,
+        username: 'jedi',
+        password: testerPassword,
+        email: 'jedi@org.com',
         is_admin: true,
         maps_api_key: process.env.SEED_MAPS_API_KEY,
       });
