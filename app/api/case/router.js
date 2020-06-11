7b7bc6c7fd5574372342fda4b6efdf8652633a33
Jeffrey Hunter <sublet@me.com>
2020-06-11 14:07:07 -0400

    Removing translation endpoints

--- app/api/case/router.js
+++ app/api/case/router.js
@@ -4,36 +4,6 @@ const server = require('../../../src/server');
 const controller = require('./controller');
 
 server.post(
-  '/case/points',
-  server.wrapAsync(async (req, res) => await controller.fetchCasePoints(req, res), true),
-);
-
-server.post(
-  '/cases/points',
-  server.wrapAsync(async (req, res) => await controller.fetchCasesPoints(req, res), true),
-);
-
-server.post(
-  '/case/points/ingest',
-  server.wrapAsync(async (req, res) => await controller.ingestUploadedPoints(req, res), true),
-);
-
-server.post(
-  '/case/point',
-  server.wrapAsync(async (req, res) => await controller.createCasePoint(req, res), true),
-);
-
-server.put(
-  '/case/point',
-  server.wrapAsync(async (req, res) => await controller.updateCasePoint(req, res), true),
-);
-
-server.post(
-  '/case/point/delete',
-  server.wrapAsync(async (req, res) => await controller.deleteCasePoint(req, res), true),
-);
-
-server.post(
   '/case/consent-to-publishing',
   server.wrapAsync(async (req, res) => await controller.consentToPublish(req, res), true),
 );
