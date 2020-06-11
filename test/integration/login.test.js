ff1698c1fbd33148b0448982a71c9dc1dd2f5487
Daniel McCoy <dan.mccoy@rocketinsights.com>
2020-06-11 18:32:20 -0400

    remove console.log in test

--- test/integration/login.test.js
+++ test/integration/login.test.js
@@ -36,7 +36,6 @@ describe('POST /login', function() {
         password: 'password',
       })
       .end(function(err, res) {
-        console.log(res.body);
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('object');
         expect(res.body).to.haveOwnProperty('token');
