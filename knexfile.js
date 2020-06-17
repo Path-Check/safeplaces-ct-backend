22554f24924ef236664664e126393f4063179bac
Daniel McCoy <dan.mccoy@rocketinsights.com>
2020-06-17 11:16:33 -0400

    add knex config for staging db

--- knexfile.js
+++ knexfile.js
@@ -34,6 +34,21 @@ module.exports = {
       directory: __dirname + '/db/seeds/development',
     },
   },
+  staging: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development',
+    },
+  },
   production: {
     client: 'pg',
     connection: {
