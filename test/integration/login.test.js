db5a60287ae3f39b344c5b2f907dadafd045e406
Aiyan Jiang <mr.aiyanj@gmail.com>
2020-06-17 11:02:46 -0400

    Increase timeout to 10s

--- test/integration/login.test.js
+++ test/integration/login.test.js
@@ -26,8 +26,8 @@ function parseJwt(token) {
 }
 
 describe('POST /login', function() {
-  this.timeout(5000);
-  
+  this.timeout(10000);
+
   it('should login on user creds and return map api key', function(done) {
     chai
       .request(server.app)
