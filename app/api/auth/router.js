b68a84f5fd2f7da1daa5b4cbc1dfde695ce9e801
Aiyan Jiang <mr.aiyanj@gmail.com>
2020-06-19 12:21:57 -0400

    Respond with cookie at login

--- app/api/auth/router.js
+++ app/api/auth/router.js
@@ -1,9 +1,5 @@
 const server = require('../../../src/server');
-const passport = require('../../../src/server/passport')
+const passport = require('../../../src/server/passport');
 const controller = require('./controller');
 
-server.post(
-  '/login',
-  passport.authenticate('ldap'),
-  controller.login
-);
+server.post('/login', passport.authenticate('ldap'), controller.login);
--- db/migrations/20200612151326_chag_timeframe.js
+++ db/migrations/20200612151326_chag_timeframe.js
@@ -1,15 +1,13 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.table('settings', table => {
     table.dropColumn('notification_threshold_count');
     table.integer('notification_threshold_timeframe').defaultTo(30);
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.table('organizations', table => {
     table.dropColumn('notification_threshold_timeframe');
     table.integer('notification_threshold_count').defaultTo(6);
   });
 };
-
