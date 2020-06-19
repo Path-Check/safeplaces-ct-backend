8c1502a7ecbd4e1be6c5ce35dfc0c144fe4228de
Daniel McCoy <MarshallMcCoy@users.noreply.github.com>
2020-06-19 19:02:45 -0400

    Revert "Respond with cookie at login (#155)" (#161)
    This reverts commit 75935d9e27a5b0cb311ef34adb3e5d80e67d759b.

--- db/migrations/20200611164840_organizations_external_id.js
+++ db/migrations/20200611164840_organizations_external_id.js
@@ -1,10 +1,11 @@
-exports.up = function (knex) {
+
+exports.up = function(knex) {
   return knex.schema.table('organizations', table => {
     table.uuid('external_id').unique();
   });
 };
 
-exports.down = function (knex) {
+exports.down = function(knex) {
   return knex.schema.table('organizations', table => {
     table.dropColumn('external_id');
   });
--- db/models/publications.js
+++ db/models/publications.js
@@ -2,31 +2,25 @@ const BaseService = require('../common/service.js');
 const casesService = require('./cases');
 
 class Service extends BaseService {
+
   findLastOne(query) {
     return this.table.where(query).orderBy('created_at', 'desc').first();
   }
 
   async findLastPublicationAndCases(options) {
-    if (!options.organization_id)
-      throw new Error('Organization ID is invalid.');
-    if (!options.status) throw new Error('Status is invalid.');
+    if (!options.organization_id) throw new Error('Organization ID is invalid.')
+    if (!options.status) throw new Error('Status is invalid.')
 
     const query = {
-      organization_id: options.organization_id,
-    };
-
-    const publication = await this.table
-      .where(query)
-      .orderBy('created_at', 'desc')
-      .first();
-    if (publication) {
-      publication.cases = await this.selectPublicationCases(
-        publication.id,
-        options.status,
-      );
-      return publication;
+      organization_id: options.organization_id
     }
-    return null;
+
+    const publication = await this.table.where(query).orderBy('created_at', 'desc').first();
+    if (publication) {
+      publication.cases = await this.selectPublicationCases(publication.id, options.status)
+      return publication
+    }
+    return null
   }
 
   async insert(publication) {
@@ -36,19 +30,17 @@ class Service extends BaseService {
     if (results) {
       return results[0];
     }
-    throw new Error('Internal server error.');
+    throw new Error('Internal server error.')
   }
 
   async selectPublicationCases(publication_id, state) {
-    const cases = await casesService.find({
-      publication_id: publication_id,
-      state,
-    });
+    const cases = await casesService.find({ publication_id: publication_id, state })
     if (cases) {
-      return cases.map(c => c.id);
+      return cases.map(c => c.id)
     }
-    return [];
+    return []
   }
+
 }
 
 module.exports = new Service('publications');
