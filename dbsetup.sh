3a2d702ecbc45f816c2ce1991e28965aee8362d7
Jeffrey Hunter <sublet@me.com>
2020-06-22 13:50:14 -0400

    That was dumb

--- dbsetup.sh
+++ dbsetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-spdl migrate:rollback --scope private --env $NODE_ENV
+spdl migrate:latest --scope private --env $NODE_ENV
 
 if [ $NODE_ENV = "development" ]; then
   spdl seed:run --scope private --env $NODE_ENV
