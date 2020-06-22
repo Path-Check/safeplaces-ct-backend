ca740e05e77d19398da38e0b4c5874f9f5a953c3
Jeffrey Hunter <sublet@me.com>
2020-06-22 11:17:17 -0400

    Small updates to dockerfile and debsetup

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ COPY --from=build-env /app /app
 WORKDIR /app
 ADD wait-for.sh /wait-for.sh
 RUN npm install -g knex
+RUN npm install -g @sublet/data-layer
 ENTRYPOINT ["/app/dbsetup.sh"]
 CMD ["npm", "start"]
 
--- dbsetup.sh
+++ dbsetup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-knex --knexfile /app/knexfile.js migrate:latest --env $NODE_ENV
+spdl migrate:rollback --scope private --env $NODE_ENV
 
 if [ $NODE_ENV = "development" ]; then
-  knex --knexfile /app/knexfile.js seed:run --env $NODE_ENV
+  spdl seed:run --scope private --env $NODE_ENV
 fi
 
 exec "$@"
