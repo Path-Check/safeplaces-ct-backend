f82d1ac1b184b62205c41d27732a6a94b8a83934
Aiyan Jiang <mr.aiyanj@gmail.com>
2020-06-24 12:33:55 -0400

    Set up Auth0 flow

--- app/api/auth/router.js
+++ app/api/auth/router.js
@@ -1,9 +1,24 @@
 const server = require('../../../src/server');
-const passport = require('../../../src/server/passport')
+const passport = require('../../../src/server/passport');
 const controller = require('./controller');
 
 server.post(
   '/login',
   passport.authenticate('ldap'),
-  controller.login
+  controller.login,
+);
+
+server.get(
+  '/auth/login',
+  controller.authLogin,
+);
+
+server.get(
+  '/auth/logout',
+  controller.authLogout,
+);
+
+server.get(
+  '/auth/callback',
+  controller.authCallback,
 );
