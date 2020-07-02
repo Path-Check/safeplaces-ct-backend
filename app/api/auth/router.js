e838b68331fdc04e6ff2e2f27d29cd9c8c971418
Aiyan Jiang <48393820+aiyan@users.noreply.github.com>
2020-07-02 15:45:06 -0400

    [Tiny change] Remove deprecated "/login" endpoint (#193)
    * Remove deprecated "/login" endpoint
    
    * Fix newman tests

--- app/api/auth/router.js
+++ app/api/auth/router.js
@@ -3,13 +3,6 @@ const controller = require('./controller');
 
 /**
  * Log in
- *
- * DEPRECATED - use "/auth/login" instead.
- */
-router.post('/login', controller.login);
-
-/**
- * Log in
  */
 router.post('/auth/login', controller.login);
 
