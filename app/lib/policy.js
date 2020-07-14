f1a3409189bbedc06234ad30cfbbb42b5792cb1e
Aiyan Jiang <48393820+aiyan@users.noreply.github.com>
2020-07-14 12:03:38 -0400

    [PLACES-467] Integrate access control to support user roles (#201)
    * Integrate access control
    
    * PLACES-475 bump data-layer-lib to 0.1.5 to fix is milliseconds/seconds issue (#202)
    
    * PLACES-475 add handling of non-unique externalIds in PUT /case (#206)
    
    * Integrate access control
    
    * Support cookie domain (#207)
    
    * bump safeplaces-auth to 0.2.2 (#208)
    
    * Integrate access control
    
    * Dependencies fix
    
    Co-authored-by: Daniel McCoy <MarshallMcCoy@users.noreply.github.com>

---
+++ app/lib/policy.js
@@ -0,0 +1,52 @@
+const xp = require('xpolicy');
+const { NotIn, NotEq, Eq, Any } = xp.rules;
+
+const enforcer = new xp.Enforcer();
+
+enforcer.addPolicy(new xp.Policy({
+  id: 1,
+  description: `Allow a contact tracer to do anything to any resource except
+    case publishing and organization configuration.`,
+  subject: Eq('contact_tracer'),
+  resource: NotIn(['/cases/publish', '/organization/configuration']),
+  action: {
+    method: Any(),
+  },
+  effect: xp.effects.Allow,
+}));
+
+enforcer.addPolicy(new xp.Policy({
+  id: 2,
+  description: `Allow a contact tracer to do anything except PUT to the
+    organization configuration.`,
+  subject: Eq('contact_tracer'),
+  resource: Eq('/organization/configuration'),
+  action: {
+    method: NotEq('PUT'),
+  },
+  effect: xp.effects.Allow,
+}));
+
+enforcer.addPolicy(new xp.Policy({
+  id: 3,
+  description: `Allow an admin to do anything to any resource.`,
+  subject: Eq('admin'),
+  resource: Any(),
+  action: {
+    method: Any(),
+  },
+  effect: xp.effects.Allow,
+}));
+
+module.exports = {
+  authorize: (role, method, path) => {
+    const op = new xp.Operation({
+      subject: role,
+      resource: path,
+      action: {
+        method,
+      }
+    });
+    return enforcer.isAllowed(op);
+  },
+};
